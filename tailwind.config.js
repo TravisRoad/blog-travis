9f9924e147a97c514c6c65cf8a0e29aebecf8772
Travis <lxymontage@gmail.com>
2022-10-15 23:55:13 +0800

    添加nord主题

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,31 @@ module.exports = {
     "./data/**/*.mdx",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nord: {
+          // https://www.nordtheme.com/docs/colors-and-palettes
+          dark: "#242933",
+          light: "#f2f4f8",
+          0: "#2e3440",
+          1: "#3b4252",
+          2: "#434c5e",
+          3: "#4c566a",
+          4: "#d8dee9",
+          5: "#e5e9f0",
+          6: "#eceff4",
+          7: "#8fbcbb",
+          8: "#88c0d0",
+          9: "#81a1c1",
+          10: "#5e81ac",
+          11: "#bf616a",
+          12: "#d08770",
+          13: "#ebcb8b",
+          14: "#a3be8c",
+          15: "#b48ead",
+        },
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
