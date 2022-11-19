3e5f394c754e68b61b4ef2fada26b98bc559a577
Travis <lxymontage@gmail.com>
2022-11-19 12:19:51 +0800

    add sitemap

---
+++ lib/sitemap.ts
@@ -0,0 +1,47 @@
+import { writeFileSync } from "fs";
+import { globby } from "globby";
+import prettier from "prettier";
+import metadata from "data/metaData";
+
+async function createSiteMap() {
+  const pages = await globby([
+    "pages/*.tsx",
+    "data/**/*.mdx",
+    "!data/*.mdx",
+    "!data/movie/*.*",
+    "!pages/_*.tsx",
+    "!pages/api",
+    "!pages/404.tsx",
+  ]);
+
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${pages
+          .map((page) => {
+            const path = page
+              .replace("pages", "")
+              .replace("data", "")
+              .replace(".tsx", "")
+              .replace("blog/", "posts/")
+              .replace(".mdx", "");
+            const route = path === "/index" ? "" : path;
+
+            return `
+              <url>
+                  <loc>${`${metadata.site}${route}`}</loc>
+              </url>
+            `;
+          })
+          .join("")}
+    </urlset>
+    `;
+  const formatted = prettier.format(sitemap, {
+    parser: "html",
+  });
+
+  // eslint-disable-next-line no-sync
+  writeFileSync("public/sitemap.xml", formatted);
+}
+
+export default createSiteMap;
