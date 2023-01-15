b46626a37b5262c5fe89e2cc7514637c067a2f31
Travis <46567145+TravisRoad@users.noreply.github.com>
2023-01-15 22:00:25 +0800

    set Image border enabled default and rounded enabled default.

--- components/mdx/MyImage.tsx
+++ components/mdx/MyImage.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
 
-export default function MyImage({ src, alt, width, height }: any) {
+export default function MyImage({
+  src,
+  alt,
+  width,
+  height,
+  border = true,
+  rounded = true,
+}: any) {
   return (
-    <div className="mx-4 mt-4 mb-6 break-inside-avoid-page">
-      <div className="grid place-items-center overflow-hidden rounded-lg border-4 border-gray-400 dark:border-2 dark:border-white/10">
+    <div className="mx-2 mt-4 mb-6 break-inside-avoid-page">
+      <div
+        className={`grid place-items-center overflow-hidden ${
+          rounded && "rounded-lg "
+        } ${
+          border &&
+          " border-[3px] border-gray-400 dark:border-2 dark:border-white/10"
+        }`}
+      >
         <Image src={`${src}`} width={width} height={height} alt={alt} />
       </div>
       {alt && (
