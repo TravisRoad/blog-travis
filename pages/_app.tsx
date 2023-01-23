f5ffc66cf312862454dbb9c82c8f2e596f9a3005
Travis <46567145+TravisRoad@users.noreply.github.com>
2023-01-23 22:50:58 +0800

    添加网站分析脚本

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -87,23 +87,6 @@ function MyApp({ Component, pageProps }: AppProps) {
           media="(prefers-color-scheme: dark)"
         /> */}
       </Head>
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-1Y104QW25K"
-      />
-          
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-1Y104QW25K');
-            `,
-        }}
-      />
       <NextNProgress
         options={{
           showSpinner: false,
