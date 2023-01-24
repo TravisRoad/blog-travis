e86d874da5af0fabe3511a13a228107233652152
Travis <46567145+TravisRoad@users.noreply.github.com>
2023-01-24 15:13:06 +0800

    add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
+import Analytics from "components/Analytics";
 
 export default function Document() {
   return (
@@ -9,35 +10,7 @@ export default function Document() {
         <Main />
         <NextScript />
         <Script src="/lib/darkmode.js" strategy="beforeInteractive" />
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-1Y104QW25K"
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-1Y104QW25K');
-            `,
-          }}
-        />
-        <Script
-          id="clarity-Microsoft"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "fitpvdaw6o");
-              `,
-          }}
-        />
+        <Analytics />
       </body>
     </Html>
   );
