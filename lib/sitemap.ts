e4fa60e828380c44cc81bac9101229d60de005c3
TravisRoad <46567145+TravisRoad@users.noreply.github.com>
2023-03-30 09:37:18 +0800

    Update sitemap.ts

--- lib/sitemap.ts
+++ lib/sitemap.ts
@@ -19,7 +19,11 @@ async function createSiteMap() {
 
   const sitemap = `
     <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
         ${pages
           .map((page) => {
             const path = page
@@ -35,7 +39,7 @@ async function createSiteMap() {
             return `
               <url>
                   <loc>${`${metadata.site}${route}`}</loc>
-                  <lastmod>${format(mtime, "yyyy-LL-dd")}</lastmod>
+                  <lastmod>${format(mtime, "yyyy-MM-dd'T'HH:mm:ssxxx")}</lastmod>
               </url>
             `;
           })
