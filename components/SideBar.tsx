870925558c2148dda9443dad7779b49f2261612e
Travis <46567145+TravisRoad@users.noreply.github.com>
2023-05-19 23:28:44 +0800

    添加twitter图标和链接

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -8,7 +8,7 @@ import metadata from "data/metaData";
 const Logo = ({ children, url }: { children: any; url: string }) => {
   return (
     <a
-      className=" rounded-lg p-1 ring-nord-11 transition-all hover:bg-nord-6 hover:ring-2 dark:hover:bg-nord-1/80"
+      className=" rounded-lg p-1 ring-nord-11 transition-all hover:bg-nord-6 hover:ring-4 dark:hover:bg-nord-1/80"
       href={url}
     >
       {children}
@@ -47,7 +47,7 @@ export default function SideBar() {
       <div className="mx-auto mt-1 text-center font-[500] "> 👨‍🎓 学习中... </div>
       {/* about */}
       <div className="mt-2 px-1">
-        <div className="my-5 mx-auto flex items-center justify-center gap-2 ">
+        <div className="my-5 mx-auto flex items-center justify-center gap-2 px-2">
           {/* github */}
           <Logo url="https://github.com/travisroad">
             <svg
@@ -72,6 +72,23 @@ export default function SideBar() {
           </Logo>
           <Logo url={`/feed/feed.xml`}>
             <RssIcon className="h-6 w-6" />
+          </Logo>
+          <Logo url="https://twitter.com/LuXiuyuan138454">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="img"
+              className="h-6 w-6"
+              width="32"
+              height="32"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M22.46 6c-.77.35-1.6.58-2.46.69c.88-.53 1.56-1.37 1.88-2.38c-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29c0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15c0 1.49.75 2.81 1.91 3.56c-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07a4.28 4.28 0 0 0 4 2.98a8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21C16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56c.84-.6 1.56-1.36 2.14-2.23Z"
+              ></path>
+            </svg>
           </Logo>
         </div>
 
