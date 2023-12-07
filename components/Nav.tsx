01ad65c421161f839d4945a065a471f93bcfd01a
Travis <lxymontage@gmail.com>
2023-12-07 11:02:43 +0800

    fix

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -10,29 +10,40 @@ export default function Nav({
   current: string;
   className: string;
 }) {
-  function NavLink({ href, key, content }: any) {
+  function NavLink({ href, content }: any) {
     return (
-      <a key={key}>
-        <Link href={`/idea/${href}`}>
-          <button
-            className={` ${
-              content === current ? "bg-nord-10/50" : "bg-nord-10/20"
-            } border-b border-b-nord-3/20 py-1 px-2 shadow-lg first:rounded-l-lg last:rounded-r-lg hover:bg-nord-10/50 dark:hover:bg-nord-10/80`}
-          >
-            {content}
-          </button>
-        </Link>
-      </a>
+      <Link href={`/idea/${href}`}>
+        <button
+          className={` ${
+            content === current ? "bg-nord-10/50" : "bg-nord-10/20"
+          } border-b border-b-nord-3/20 py-1 px-2 shadow-lg first:rounded-l-lg last:rounded-r-lg hover:bg-nord-10/50 dark:hover:bg-nord-10/80`}
+        >
+          {content}
+        </button>
+      </Link>
     );
   }
 
+  // if (pages.length >= 5) {
+  //   pages = [...pages.slice(0, 3), "...", ...pages.slice(-2)];
+  // }
+
+  const lz = [
+    ["1", "first", "<<"],
+    ...pages.map((page, i) => {
+      if (page === "...") {
+        return [i + 1, i + 1, page];
+      }
+      return [page, page, page];
+    }),
+    [pages[pages.length - 1], "last", ">>"],
+  ];
+
   return (
-    <div className={`${className} divide-x-2 font-mono dark:divide-nord-5/20`}>
-      <NavLink href="1" key="first" content={"<<"} />
-      {pages.map((page) => (
-        <NavLink href={`${page}`} key={page} content={page} />
+    <div className={`${className} divide-x-2 font-mono dark:divide-x-0 `}>
+      {lz.map(([href, key, content]) => (
+        <NavLink href={href} key={key} content={content} />
       ))}
-      <NavLink href={pages[pages.length - 1]} key="last" content={">>"} />
     </div>
   );
 }
