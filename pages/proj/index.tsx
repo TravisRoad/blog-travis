bb4a79807b7b645aaf2ca20635ba6cfdb78a604f
travis <lxymontage@gmail.com>
2024-12-27 18:03:14 +0800

    2024 recap

--- pages/proj/index.tsx
+++ pages/proj/index.tsx
@@ -16,7 +16,7 @@ export default function Proj() {
       <h1 className="sr-only" id="forSearchEngine">
         Proj | 项目
       </h1>
-      <div className="prose prose-stone mx-auto flex w-full flex-col prose-h2:text-lg prose-a:text-nord-9 prose-a:no-underline hover:prose-a:underline dark:prose-invert sm:max-w-3xl ">
+      <div className="prose prose-stone mx-auto mt-2 flex w-full flex-col prose-h2:text-lg prose-a:text-nord-9 prose-a:no-underline hover:prose-a:underline dark:prose-invert sm:max-w-3xl ">
         <Content components={MDXComponents} />
       </div>
     </Main>
