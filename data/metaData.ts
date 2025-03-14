f3ba90672f8e72ef540738ec3ccd0dd121f700d3
travis <lxymontage@gmail.com>
2025-03-15 01:14:34 +0800

    fix

--- data/metaData.ts
+++ data/metaData.ts
@@ -26,7 +26,7 @@ export const settings = {
 export const pageRouter: { url: string; name: string; nameEn: string }[] = [
   { url: "/", name: "主页", nameEn: "Home" },
   { url: "/posts", name: "文章", nameEn: "Posts" },
-  { url: "/travelogue", name: "游记", nameEn: "story" },
+  { url: "/travelogue", name: "游记", nameEn: "Travelogues" },
   { url: "/idea/1", name: "想法", nameEn: "Ideas" },
   { url: "/about", name: "关于", nameEn: "About" },
   // { url: "https://www.travellings.cn/go.html", name: "开往", nameEn: "Travel" },
